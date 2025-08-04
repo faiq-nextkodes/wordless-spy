413a4d699df5e1306f9d08f828ad8a0689c8dabd
Faiq <your.faiq@nextkodes.com>
2025-08-04 15:40:04 +0500

    BootstrapSetup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "bootstrap"
+// Optional: Initialize all Bootstrap components
+document.addEventListener("DOMContentLoaded", function() {
+// Initialize Bootstrap tooltips
+var tooltipTriggerList = [ ].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+})
